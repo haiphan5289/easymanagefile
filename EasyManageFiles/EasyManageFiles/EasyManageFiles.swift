93546edcd2a6f9305f18a9881b36990e2a2530c3
haiphan5289 <haiphan5289@gmail.com>
2022-08-03 20:56:24 +0700

    init again

---
+++ EasyManageFiles/EasyManageFiles/EasyManageFiles.swift
@@ -0,0 +1,16 @@
+//
+//  EasyManageFiles.swift
+//  EasyManageFiles
+//
+//  Created by haiphan on 03/08/2022.
+//
+
+import Foundation
+
+public struct CheckFW {
+    
+    public static func sayHello() {
+        print("==== Say Hello=======")
+    }
+    
+}
